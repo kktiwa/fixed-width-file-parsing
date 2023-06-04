9273f670720341c282f701e1ee78d6d803f5b64a
kunaltiwary <kunaltiwary@gmail.com>
2023-06-04 15:37:09 +1000

    Added a fixed-width file generator

--- src/main/scala/com/lfs/parser/generator/FixedWidthFileGenerator.scala
+++ src/main/scala/com/lfs/parser/generator/FixedWidthFileGenerator.scala
@@ -1,5 +1,49 @@
 package com.lfs.parser.generator
 
+import com.lfs.parser.config.Column
+
 object FixedWidthFileGenerator {
+
+  def generate(columns: List[Column], numberOfLines: Int): List[String] =
+    generateColumnHeading(columns) +:
+      (1 to numberOfLines)
+        .toList
+        .map(_ => generateLine(columns))
+
+  private def generateLine(columns: List[Column]): String =
+    columns
+      .map(
+        column => generateColumnValue(column)
+      ).mkString
+
+  /**
+   * @param columns
+   * @return
+   */
+  private def generateColumnHeading(columns: List[Column]): String =
+    columns
+      .map(
+        column => addPadding(column.name, column)
+      ).mkString
+
+  /**
+   * Add padding to ensure keeping the column width fixed
+   *
+   * @param content
+   * @param column
+   * @return
+   */
+  private def addPadding(content: String, column: Column): String = {
+    //Column names can be longer than their width but not their content values
+    val width = Math.max(column.width, column.name.length + 1)
+    //Return the given string concatenated `n` times.
+    content + " " * (width - content.length)
+  }
+
+  private def generateColumnValue(column: Column): String = {
+    //Return the given string concatenated `n` times.
+    val content = "X" * (column.width - 1)
+    addPadding(content, column)
+  }
 
 }
--- src/test/scala/com/lfs/parser/generator/FixedWidthFileGeneratorTest.scala
+++ src/test/scala/com/lfs/parser/generator/FixedWidthFileGeneratorTest.scala
@@ -1,5 +1,42 @@
 package com.lfs.parser.generator
 
-class FixedWidthFileGeneratorTest {
+import org.scalatest.flatspec.AnyFlatSpec
+import com.lfs.parser.config.Column
+import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
+
+class FixedWidthFileGeneratorTest extends AnyFlatSpec {
+
+  "FixedWidthFileGenerator" should "generate 1 line correctly with headers" in {
+    val columns = List(Column("a", 2), Column("b", 3), Column("c", 4))
+    val linesGenerated = FixedWidthFileGenerator.generate(columns, 1)
+    linesGenerated.size shouldBe 2
+    linesGenerated(0) shouldBe "a b  c   "
+    linesGenerated(1) shouldBe "X XX XXX "
+  }
+
+  it should "generate 2 lines correctly with headers" in {
+    val columns = List(Column("a", 2), Column("b", 3), Column("c", 4))
+    val linesGenerated = FixedWidthFileGenerator.generate(columns, 2)
+    linesGenerated.size shouldBe 3
+    linesGenerated(0) shouldBe "a b  c   "
+    linesGenerated(1) shouldBe "X XX XXX "
+    linesGenerated(2) shouldBe "X XX XXX "
+  }
+
+  it should "generate column names greater than fixed width" in {
+    val columns = List(Column("aaaaa", 2), Column("b", 3), Column("c", 4))
+    val linesGenerated = FixedWidthFileGenerator.generate(columns, 1)
+    linesGenerated.size shouldBe 2
+    linesGenerated(0) shouldBe "aaaaa b  c   "
+    linesGenerated(1) shouldBe "X     XX XXX "
+  }
+
+  it should "generate column names same as column width" in {
+    val columns = List(Column("aa", 2), Column("bb", 2), Column("ccc", 3))
+    val linesGenerated = FixedWidthFileGenerator.generate(columns, 1)
+    linesGenerated.size shouldBe 2
+    linesGenerated(0) shouldBe "aa bb ccc "
+    linesGenerated(1) shouldBe "X  X  XX  "
+  }
 
 }
