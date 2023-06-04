9ccc94dfc306074d19cef0ef996f9155d9ca4c47
kunaltiwary <kunaltiwary@gmail.com>
2023-06-04 16:30:46 +1000

    Added CsvParser logic

--- src/main/scala/com/lfs/parser/csv/CsvParser.scala
+++ src/main/scala/com/lfs/parser/csv/CsvParser.scala
@@ -1,5 +1,20 @@
 package com.lfs.parser.csv
 
+import com.lfs.parser.config.Column
+
 object CsvParser {
+
+  def parse(columns: List[Column], lines: List[String]): Seq[String] = {
+    val columnWidths = columns.map(column => Math.max(column.width, column.name.length + 1))
+    //E.g: List(List(0, 2), List(2, 5), List(5, 9))
+    val offsetStartEndPairs = columnWidths.scanLeft(0)(_ + _).sliding(2).toList
+    lines.map(line => parseLine(offsetStartEndPairs, line))
+  }
+
+  private def parseLine(offsets: List[List[Int]], line: String): String = {
+    offsets.map {
+      case List(start, end) => line.substring(start, end).trim
+    }.mkString(",")
+  }
 
 }
--- src/test/scala/com/lfs/parser/csv/CsvParserTest.scala
+++ src/test/scala/com/lfs/parser/csv/CsvParserTest.scala
@@ -1,5 +1,22 @@
 package com.lfs.parser.csv
 
-class CsvParserTest {
+import org.scalatest.flatspec.AnyFlatSpec
+import com.lfs.parser.config.Column
+import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
+
+class CsvParserTest extends AnyFlatSpec {
+
+  "CsvParser" should "parse valid lines correctly" in {
+    val columns = List(Column("a", 2), Column("b", 3), Column("c", 4))
+    val lines = List(
+      "a b  c   ",
+      "X XX XXX ",
+      "X XX XXX "
+    )
+    val csvLines = CsvParser.parse(columns, lines)
+    csvLines(0) shouldBe "a,b,c"
+    csvLines(1) shouldBe "X,XX,XXX"
+    csvLines(2) shouldBe "X,XX,XXX"
+  }
 
 }
